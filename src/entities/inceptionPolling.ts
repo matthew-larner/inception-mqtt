2171893651a0f289f701dfa26f5bedd91993faeb
Neil Palima <neil@clicksend.com>
2020-11-08 14:07:40 +0800

    update public state checking

--- src/entities/inceptionPolling.ts
+++ src/entities/inceptionPolling.ts
@@ -8,20 +8,20 @@ export const polling = async () => {
 
     console.log(`Polling area received id '${id}' with public state '${publicState}'`);
 
-    if ([1, 256].includes(publicState)) {
+    if (publicState === 257) {
       message = 'armed_away';
-    } else if (publicState === 2) {
+    } else if (publicState === 259) {
       message = 'triggered';
-    } else if ([4, 16].includes(publicState)) {
+    } else if (publicState === 261) {
       message = 'pending';
-    } else if (publicState === 8) {
+    } else if (publicState === 265) {
       message = 'arming';
-    } else if ([32, 2048].includes(publicState)) {
-      message = 'disarmed';
-    } else if (publicState === 512) {
+    } else if (publicState === 513) {
       message = 'armed_home';
-    } else if (publicState === 1024) {
+    } else if (publicState === 1025) {
       message = 'armed_night';
+    } else if (publicState === 2048) {
+      message = 'disarmed';
     } else {
       // ignore if unexpected public state
       return;
@@ -37,7 +37,7 @@ export const polling = async () => {
 
     if ([1, 2, 8].includes(publicState)) {
       message = 'On';
-    } else if ([4, 64].includes(publicState)) {
+    } else if ([4, 64, 68].includes(publicState)) {
       message = 'Off';
     } else {
       // ignore if unexpected public state
@@ -69,9 +69,9 @@ export const polling = async () => {
 
     console.log(`Polling door received id '${id}' with public state '${publicState}'`);
 
-    if (publicState === 1) {
+    if (publicState === 3) {
       message = 'UNLOCKED';
-    } else if (publicState === 256) {
+    } else if (publicState === 258) {
       message = 'LOCKED';
     } else {
       // ignore if unexpected public state
