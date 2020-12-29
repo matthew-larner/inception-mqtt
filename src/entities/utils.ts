c6d5062ebc4382251f81aff6022bd37de80e76a6
Neil Palima <neil@clicksend.com>
2020-12-29 08:38:09 +0800

    update padding function

--- src/entities/utils.ts
+++ src/entities/utils.ts
@@ -1,7 +1,12 @@
 export const numberToBinaryStringWithZeroPadding = (num: number, maxPadding: number): string => {
   const value = num.toString(2);
+  const valueLength = value.length;
 
-  return '0'.repeat(maxPadding - value.length) + value;
+  if (valueLength > maxPadding) {
+    return value.slice(valueLength - maxPadding);
+  }
+
+  return '0'.repeat(maxPadding - valueLength) + value;
 };
 
 export const isStringIndexContains = (str: string, index: number, char: string) => str[index - 1] === char; // index here uses 1 indexing
